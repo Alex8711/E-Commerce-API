61a01562450e90aad17d568ea99f14680907d1b8
Alex Liu <liuzhendell@gmail.com>
2021-01-19 21:18:11 -0700

    add one test api

---
+++ E-Commerce-API/Database/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using E_Commerce_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce_API.Database
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
